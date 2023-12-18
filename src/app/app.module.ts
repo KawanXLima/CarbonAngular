f06c6b282f8c0c528544e33bd489ff405ea829a2
Kawan Ximendes <kawan.ximendes@gmail.com>
2023-12-18 10:06:19 -0300

    initial  project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import {
+  AccordionModule,
+  ButtonModule,
+  DropdownModule,
+  IconModule,
+  SearchModule,
+  UIShellModule,
+  ThemeModule,
+  NotificationModule,
+} from 'carbon-components-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UIShellModule,
+    IconModule,
+    SearchModule,
+    ButtonModule,
+    ThemeModule,
+    DropdownModule,
+    AccordionModule,
+    NotificationModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
